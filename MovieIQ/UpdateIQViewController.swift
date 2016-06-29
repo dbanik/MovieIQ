05a310578e0c3de7dae9a100fb64aa1d16a7a718
Debaprio Banik <debapriobanik@gmail.com>
2016-06-28 18:49:44 -0700

    Implemented fetching & parsing logic

--- MovieIQ/UpdateIQViewController.swift
+++ MovieIQ/UpdateIQViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+let getURLStr = "https://www.omdbapi.com/?t=%@&y=&plot=short&r=json"
 
-class UpdateIQViewController: UIViewController, UITextFieldDelegate {
+class UpdateIQViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource {
     
-    // MARK: Properties
+    // MARK: Outlets
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var directorLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
@@ -18,20 +19,95 @@ class UpdateIQViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var actorListTableView: UITableView!
     
+    // MARK: Properties
+    var movieTitle: NSString?
+    var actorList: NSArray?
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad();
+        self.titleTextField.delegate = self;
     }
     
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning();
     }
     
-    internal func textFieldShouldReturn(textField: UITextField) -> Bool {
+    // MARK: TextField Delagate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if let text = self.titleTextField.text {
+            self.movieTitle = text;
+            self.fetchMoviedetails();
+        }
+        self.titleTextField.text = "";
+        self.titleTextField.resignFirstResponder();
         return true;
     }
     
+    // MARK: Fetching
+    func fetchMoviedetails() {
+        let urlString = NSString(format: getURLStr, self.movieTitle!);
+        let getURL = NSURL(string: urlString as String);
+        
+        var dataTask: NSURLSessionDataTask?
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true;
+        dataTask = NSURLSession.sharedSession().dataTaskWithURL(getURL!) {
+            data, response, error in
+            
+            if let error = error {
+                print(error.localizedDescription);
+                dispatch_async(dispatch_get_main_queue()) {
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false;
+                }
+            }
+            else if let httpResponse = response as? NSHTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    self.populateView(data)
+                }
+            }
+        }
+        
+        dataTask?.resume();
+    }
+    
+    // MARK: Parsing & Population
+    func populateView(data: NSData!) -> Void {
+        
+        do {
+            let json = try NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions(rawValue: 0));
+            guard let result :NSDictionary = json as? NSDictionary else {
+                print("Not a Dictionary")
+                return
+            }
+            print("JSONDictionary! \(result)")
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                self.directorLabel.text = result["Director"] as? String;
+                self.yearLabel.text = result["Year"] as? String;
+                let actor :NSString = (result["Actors"] as? NSString)!;
+                self.actorList = actor.componentsSeparatedByString(",");
+                self.actorListTableView.reloadData();
+            }
+        }
+        catch let error as NSError {
+            print("\(error)")
+        }
+    }
+    
+    // MARK: TableView Datasource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let list = self.actorList {
+            return list.count;
+        }
+        else {
+            return 0;
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Actor Cell", forIndexPath: indexPath);
+        cell.textLabel?.text = self.actorList![indexPath.row] as? String;
+        return cell;
+    }
     
 }
 
