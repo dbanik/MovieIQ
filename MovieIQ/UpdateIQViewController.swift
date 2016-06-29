f0b65dae8bacd6d690029734333dd863c7136455
Debaprio Banik <debapriobanik@gmail.com>
2016-06-28 22:53:31 -0700

    UI customisation

--- MovieIQ/UpdateIQViewController.swift
+++ MovieIQ/UpdateIQViewController.swift
@@ -12,12 +12,17 @@ let getURLStr = "https://www.omdbapi.com/?t=%@&y=&plot=short&r=json"
 class UpdateIQViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource {
     
     // MARK: Outlets
+    @IBOutlet weak var directorField: UILabel!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var directorLabel: UILabel!
+    @IBOutlet weak var yearField: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var ratingField: UILabel!
     @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var actorField: UILabel!
     @IBOutlet weak var actorListTableView: UITableView!
+    @IBOutlet weak var plotLabel: UILabel!
     
     // MARK: Properties
     var movieTitle: NSString?
@@ -49,7 +54,20 @@ class UpdateIQViewController: UIViewController, UITextFieldDelegate, UITableView
         let getURL = NSURL(string: urlString as String);
         
         var dataTask: NSURLSessionDataTask?
-        UIApplication.sharedApplication().networkActivityIndicatorVisible = true;
+        dispatch_async(dispatch_get_main_queue()) {
+            self.directorField.hidden = true;
+            self.directorLabel.hidden = true;
+            self.actorField.hidden = true;
+            self.actorListTableView.hidden = true;
+            self.yearField.hidden = true;
+            self.yearLabel.hidden = true;
+            self.ratingField.hidden = true;
+            self.ratingLabel.hidden = true;
+            self.posterView.hidden = true;
+            self.plotLabel.hidden = true;
+            
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = true;
+        }
         dataTask = NSURLSession.sharedSession().dataTaskWithURL(getURL!) {
             data, response, error in
             
@@ -63,7 +81,11 @@ class UpdateIQViewController: UIViewController, UITextFieldDelegate, UITableView
                 if httpResponse.statusCode == 200 {
                     if data != nil {
                         if let result = self.parseJSONData(data) {
-                            self.fetchMoviePoster(result);
+                            guard let errorStr: String = result["Error"] as? String else {
+                                self.fetchMoviePoster(result);
+                                return;
+                            }
+                            print("\(errorStr)");
                         }
                     }
                 }
@@ -119,12 +141,24 @@ class UpdateIQViewController: UIViewController, UITextFieldDelegate, UITableView
     // MARK: Populate UI
     func populateView(result: NSDictionary, img: UIImage) {
         dispatch_async(dispatch_get_main_queue()) {
+            self.directorField.hidden = false;
+            self.directorLabel.hidden = false;
             self.directorLabel.text = result["Director"] as? String;
+            self.yearField.hidden = false;
+            self.yearLabel.hidden = false;
             self.yearLabel.text = result["Year"] as? String;
             let actor :NSString = (result["Actors"] as? NSString)!;
             self.actorList = actor.componentsSeparatedByString(",");
+            self.actorField.hidden = false;
+            self.actorListTableView.hidden = false;
             self.actorListTableView.reloadData();
+            self.ratingField.hidden = false;
+            self.ratingLabel.hidden = false;
+            self.ratingLabel.text = result["imdbRating"] as? String;
+            self.posterView.hidden = false;
             self.posterView.image = img;
+            self.plotLabel.hidden = false;
+            self.plotLabel.text = result["Plot"] as? String;
             UIApplication.sharedApplication().networkActivityIndicatorVisible = false;
         }
     }
